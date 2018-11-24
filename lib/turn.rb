f00fee1e3627fd057c236004e10743e4adfcbc94
Songyishu Yang <songyishu.yang@gmail.com>
2018-11-24 19:08:31 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,20 +6,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def position_taken?(board, position)
-  if board[position] == " " || board[position] == "" || board[position] == nil
-  	false
-  else
-  	true
-  end
-end
-
 def valid_move?(board, position)
- 	spot = position.to_i - 1
- 	if !position_taken?(board, spot) && spot.between?(0,8)
- 		true
+ 	position_taken = position.to_i - 1
+  if board[position_taken] == " " || board[position_taken] == "" || board[position_taken] == nil
+    true
   else
- 		false
+    false
   end
 end
 
